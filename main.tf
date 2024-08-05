eba9517e7d78773374b7ad97ffd62c4d1dc13fc3
Ben Monaghan <monoganog@gmail.com>
2024-08-05 16:41:04 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -57,7 +57,7 @@ resource "aws_security_group" "rds" {
 
 resource "aws_db_parameter_group" "education" {
   name   = "${random_pet.random.id}-education"
-  family = "postgres15"
+  family = "postgres16"
 
   parameter {
     name  = "log_connections"
